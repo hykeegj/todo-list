47e8c7c53dfe37ef00a31a7822740ef1413df543
Heo SuYeong <hykeegj@gmail.com>
2020-12-05 14:36:48 +0900

    이미지 상대경로 수정중

--- bg.js
+++ bg.js
@@ -4,7 +4,7 @@ const IMG_NUMBER = 6;
 
 function paintImage(imgNumber) {
   const image = new Image();
-  image.src = `./Images/${imgNumber}.jpg`;
+  image.src = `./images/${imgNumber}.jpg`;
   image.classList.add("bgImage");
   body.prepend(image);
 }
